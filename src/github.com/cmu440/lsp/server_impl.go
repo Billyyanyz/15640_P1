cc213f53045230b88d65dabba4c1866bbe3a42b7
Billy Yan <yanyz18@mails.tsinghua.edu.cn>
2022-09-18 21:14:01 -0400

    Server Basic Structure 1

--- src/github.com/cmu440/lsp/server_impl.go
+++ src/github.com/cmu440/lsp/server_impl.go
@@ -2,10 +2,18 @@
 
 package lsp
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/cmu440/lspnet"
+	"strconv"
+)
 
 type server struct {
-	// TODO: Implement this!
+	udpConn lspnet.UDPConn
+
+	stopConnectionRoutine chan bool
+	stopMainRoutine       chan bool
 }
 
 // NewServer creates, initiates, and returns a new server. This function should
@@ -15,7 +23,62 @@ type server struct {
 // project 0, etc.) and immediately return. It should return a non-nil error if
 // there was an error resolving or listening on the specified port number.
 func NewServer(port int, params *Params) (Server, error) {
-	return nil, errors.New("not yet implemented")
+	s := &server{
+		stopConnectionRoutine: make(chan bool),
+	}
+	if addr, err := lspnet.ResolveUDPAddr("udp",
+		lspnet.JoinHostPort("localhost", strconv.Itoa(port))); err != nil {
+		return nil, err
+	} else {
+		if conn, err := lspnet.ListenUDP("udp", addr); err != nil {
+			return nil, err
+		} else {
+			s.udpConn = *conn
+			go s.connectionRoutine()
+			go s.MainRoutine()
+		}
+	}
+	return s, nil
+}
+
+func (s *server) connectionRoutine() {
+	for {
+		select {
+		case <-s.stopConnectionRoutine:
+			return
+		default:
+			var b []byte
+			if n, addr, err := s.udpConn.ReadFromUDP(b); err != nil {
+				return
+			} else {
+				bTrim := b[:n]
+				var message Message
+				if err := json.Unmarshal(bTrim, &message); err != nil {
+				} else {
+					switch message.Type {
+					case MsgConnect:
+
+					case MsgAck:
+
+					case MsgCAck:
+
+					case MsgData:
+					}
+				}
+
+			}
+
+		}
+	}
+}
+
+func (s *server) MainRoutine() {
+	for {
+		select {
+		case <-s.stopMainRoutine:
+			return
+		}
+	}
 }
 
 func (s *server) Read() (int, []byte, error) {
@@ -33,5 +96,7 @@ func (s *server) CloseConn(connId int) error {
 }
 
 func (s *server) Close() error {
+	s.stopConnectionRoutine <- true
+	s.stopMainRoutine <- true
 	return errors.New("not yet implemented")
 }
